014547ad6b95d3cb417cba11ece926d82fa0b6c8
AxelMello1998 <alessandromelotti1998@gmail.com>
2025-05-30 12:26:34 +0200

    init

---
+++ src/main/java/com/progetto_medusa/mail_service/MailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.progetto_medusa.mail_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progetto_medusa/mail_service/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progetto_medusa.mail_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
